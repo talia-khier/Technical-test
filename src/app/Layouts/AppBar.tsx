182b82811d69539400b3b554bedfe91c0279625a
Khier Talia <gk_talia@esi.dz>
2023-02-28 23:18:12 +0100

    create app bar

---
+++ src/app/Layouts/AppBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './appBar.module.css';
+
+const AppBar = () => {
+  return (
+    <div className={styles.appBar}>
+      <div>
+        <img src="hambergermenu.svg" alt="hambergermenu" />
+      </div>
+      <div>
+        <img src="notification.svg" alt="notification" />
+        <img src="moon.svg" alt="moon" />
+        <img src="image.png" alt="user" />
+      </div>
+    </div>
+  );
+};
+
+export default AppBar;

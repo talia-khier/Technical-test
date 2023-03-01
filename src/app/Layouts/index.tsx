b0782630943b9d2288cd1ce3f30ff883e215f62a
Khier Talia <gk_talia@esi.dz>
2023-03-01 13:45:35 +0100

    apply dark theme

--- src/app/Layouts/index.tsx
+++ src/app/Layouts/index.tsx
@@ -1,16 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from './AppBar';
 import Sidebar from './Sidebar';
 
 import styles from './layout.module.css';
-import useLocalStorage from '../hooks/useLocalStorage';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<string>('light');
+
+  useEffect(() => {
+    setTheme(
+      localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light'
+    );
+
+    return () => {
+      localStorage.setItem('theme', theme);
+    };
+  }, []);
+
+  const switchTheme = () => {
+    setTheme((oldTheme) => (oldTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <div className={styles.layout} data-theme="dark">
+    <div className={styles.layout} data-theme={theme}>
       <Sidebar />
       <div className={styles.contentWrapper}>
-        <AppBar />
+        <AppBar handleToggleTheme={switchTheme} />
         <div className={styles.content}>{children}</div>
       </div>
     </div>
